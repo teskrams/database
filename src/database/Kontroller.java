feb94d355b2b530f3ffc6ac626b523f7d7002480
teskrams <teskrams@stud.ntnu.no>
2021-03-19 10:22:47 +0100

    fix user

--- src/database/Kontroller.java
+++ src/database/Kontroller.java
@@ -2,8 +2,6 @@ package database;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.Collection;
 
 
 public class Kontroller extends DBConn{
@@ -11,24 +9,18 @@ public class Kontroller extends DBConn{
     public Kontroller(){
     }
 
-    static Collection<User> innlogget = new ArrayList<User>();
+    static User user;
     
 
     public void login(String username, String password) {
         try{
             PreparedStatement checkUser = conn.prepareStatement("select email, passwd from siteuser");
-            //checkUser.setString(1, "theodor@theodor.no");
-            //System.out.println("haha1");
-            //checkUser.setString(2, "passwd");
-            //checkUser.setString(3, "siteUser");
-            //System.out.println("kada");
-            //System.out.println(checkUser.toString());
             ResultSet brukere = checkUser.executeQuery();
             while (brukere.next()) {
                 if (brukere.getString("email").equals(username)) {
                     if (brukere.getString("passwd").equals(password)) {
-                        innlogget.add(new User(username, password));
-                        System.out.println("du er logget inn");
+                        user = new User(username, password);
+                        System.out.println("Du er logget inn");
                         return;
                     }
                     System.out.println("feil passord");
@@ -46,6 +38,6 @@ public class Kontroller extends DBConn{
         Kontroller test = new Kontroller();
         test.connect();
         test.login("jens@jens.no", "admin");
-        System.out.println(innlogget);
+        System.out.println(user);
     }
 }
