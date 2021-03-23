140e0bec170aa497f3ac68cd54e702898649e1a7
teskrams <teskrams@stud.ntnu.no>
2021-03-23 12:23:15 +0100

    ferdig use4 nesten use5
    Co-authored-by: jenshle <jenshle@users.noreply.github.com>

--- src/database/Kontroller.java
+++ src/database/Kontroller.java
@@ -3,6 +3,9 @@ package database;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 public class Kontroller extends DBConn{
@@ -12,7 +15,6 @@ public class Kontroller extends DBConn{
 
     static User user;
     
-
     public void login(String username, String password) {
         try{
             PreparedStatement checkUser = conn.prepareStatement("select email, passwd from siteuser");
@@ -87,7 +89,6 @@ public class Kontroller extends DBConn{
         String sqlString2 = "insert into thread values(?,?,?,?);"; 
         try {
             PreparedStatement newPost = conn.prepareStatement(sqlString);
-            System.out.println("test");
             newPost.setInt(1, findPostID());
             newPost.setString(2, heading);
             newPost.setString(3, text);
@@ -95,6 +96,7 @@ public class Kontroller extends DBConn{
             newPost.setString(5, user.getName());
             newPost.toString();            
             newPost.executeUpdate();
+            System.out.println("Posten er skrevet");
 
             if (!checkFolder(folder)) {
                 String sqlString3 = "insert into folder values(?,?)";
@@ -102,6 +104,7 @@ public class Kontroller extends DBConn{
                 newPost3.setString(1, folder.getName());
                 newPost3.setInt(2, 1);
                 newPost3.executeUpdate();
+                System.out.println("Du har laget en ny mappe med navnet:"+folder.getName());
             }
             
             PreparedStatement newPost2 = conn.prepareStatement(sqlString2);
@@ -109,8 +112,8 @@ public class Kontroller extends DBConn{
             newPost2.setInt(2, findPostID() - 1);
             newPost2.setString(3, postType);
             newPost2.setString(4, folder.getName());
-            System.out.println("test2");
             newPost2.executeUpdate();
+            System.out.println("Posten din er en ny tråd");
 
         }
         catch (Exception e) {
@@ -130,11 +133,13 @@ public class Kontroller extends DBConn{
             newPost2.setString(3, text);
             newPost2.setString(5, user.getName());
             newPost2.executeUpdate();
+            System.out.println("Posten er skrevet");
 
             PreparedStatement newPost = conn.prepareStatement(sqlString);
             newPost.setInt(1, postid);
             newPost.setInt(2, findPostID() - 1);
             newPost.executeUpdate();
+            System.out.println("Du har svart på en tråd");
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -152,13 +157,14 @@ public class Kontroller extends DBConn{
             newPost2.setString(3, text);
             newPost2.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
             newPost2.setString(5, user.getName());
-            System.out.println(newPost2.toString());
             newPost2.executeUpdate();
+            System.out.println("Posten er skrevet");
 
             PreparedStatement newPost = conn.prepareStatement(sqlString);
             newPost.setInt(1, postid);
             newPost.setInt(2, findPostID()-1);
             newPost.executeUpdate();
+            System.out.println("Du har svart på et svar");
 
         }
         catch (Exception e) {
@@ -167,12 +173,91 @@ public class Kontroller extends DBConn{
         }
     }
     
+    public Collection<Integer> searchHeading(String heading) {
+        Collection<Integer> result = new ArrayList<>();
+        try {
+            PreparedStatement headingSearch = conn.prepareStatement("select postid from post where heading like ?");
+            headingSearch.setString(1, "%"+heading+"%");
+            ResultSet posts = headingSearch.executeQuery();
+            while (posts.next()) {
+                result.add(posts.getInt("postID"));
+            }
+            System.out.println("Søket ga følgende treff:"+result.toString());
+            return result;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("feil i databaseconnection");
+            return result;
+        }
+    }
+
+    public Collection<Integer> searchText(String text) {
+        Collection<Integer> result = new ArrayList<>();
+        try {
+            PreparedStatement textSearch = conn.prepareStatement("select postid from post where postText like ?");
+            textSearch.setString(1,"%" + text + "%");
+            ResultSet posts = textSearch.executeQuery();
+            while (posts.next()){
+                result.add(posts.getInt("postid"));
+            }
+            System.out.println("Søket ga følgende treff: " + result.toString());
+            return result;
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("feil i databaseconnection");
+            return result;
+        }
+    }
+
+    public Collection<List<String>> stats() {
+        Collection<List<String>> result = new ArrayList<>();
+        try {
+            PreparedStatement stats = conn.prepareStatement("select siteUser.email, count(hasread.email)"+
+                                                            "from (siteUser left outer join hasread on siteUser.email = hasread.Email)"+
+                                                            "group by hasread.emailorder by count(hasread.email) desc;");
+            ResultSet read = stats.executeQuery();
+            Collection<List<String>> readlist = new ArrayList<>();
+            while (read.next()) {
+                List<String> løkke = new ArrayList<>();
+                løkke.add(read.getString("email"));
+                løkke.add(read.getString("count(hasread.email)"));
+                readlist.add(løkke);
+            }
+            PreparedStatement stats2 = conn.prepareStatement("select siteUser.email, count(post.authoremail)"+
+                                                            "from (siteUser left outer join post on siteUser.email = post.authorEmail)"+
+                                                            "group by post.authoremail order by count(post.authoremail) desc;");
+            ResultSet write = stats2.executeQuery();
+            List<List<String>> writelist = new ArrayList<>();
+            while (write.next()) {
+                List<String> løkke = new ArrayList<>();
+                løkke.add(read.getString("email"));
+                løkke.add(read.getString("count(hasread.email)"));
+                writelist.add(løkke);
+            }
+            result.addAll(readlist);
+            for (List<String> list : writelist) {
+                result.stream().forEach(e -> e.get(0).equals(list.get(0)));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("feil i databaseconnection");
+            return result;
+        }
+        return result;
+    }
+    
+    
     public static void main(String[] args) {
         Kontroller test = new Kontroller();
         test.connect();
         test.login("jens@jens.no", "admin");
-        test.newPost(user, "test1", "test teste teste", new Folder("test"), "Question");
+        //test.newPost(user, "test1", "test teste teste", new Folder("test"), "Question");
         System.out.println(user);
+        test.searchHeading("tt");
+        test.searchText("teste");
         //test.replyReply(user, "testtest", "kultest", 1);
         //System.out.println(test.findPostID());
     }
